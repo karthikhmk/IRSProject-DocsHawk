a95223af1371f7be973208f56da9852a47b3670e
bhavyabatra <bhavyabatra88@gmail.com>
2016-03-07 11:51:19 -0600

    Web Service_Receptor
    WBSVC

---
+++ BB/BB/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="BB.WebForm1" %>
+
+<!DOCTYPE html>
+
+
+<HTML>
+  <HEAD>
+    <title>WebForm1</title>
+    <meta name="GENERATOR" Content="Microsoft Visual Studio 7.0">
+    <meta name="CODE_LANGUAGE" Content="C#">
+    <meta name=vs_defaultClientScript content="JavaScript">
+    <meta name=vs_targetSchema content="http://schemas.microsoft.com/intellisense/ie5">
+  </HEAD>
+  <body MS_POSITIONING="GridLayout">
+<form id="Form1" method="post" enctype="multipart/form-data" runat="server">
+<INPUT type=file id=File1 name=File1 runat="server" >
+<br>
+<input type="submit" id="Submit1" value="Upload" runat="server" NAME="Submit1">
+</form>
+
+	
+  </body>
+</HTML>
---
+++ BB/BB/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace BB
+{
+    /// <summary>
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected System.Web.UI.HtmlControls.HtmlInputFile File1;
+        protected System.Web.UI.HtmlControls.HtmlInputButton Submit1;
+
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+             
+            InitializeComponent();
+            base.OnInit(e);
+        }
+
+        /// <summary>
+     
+        private void InitializeComponent()
+        {
+            this.Submit1.ServerClick += new System.EventHandler(this.Submit1_ServerClick);
+            this.Load += new System.EventHandler(this.Page_Load);
+
+        }
+        #endregion
+
+        private void Submit1_ServerClick(object sender, System.EventArgs e)
+        {
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Data") + "\\" + fn;
+                try
+                {
+                    File1.PostedFile.SaveAs(SaveLocation);
+                    Response.Write("The file has been uploaded.");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                    
+                }
+            }
+            else
+            {
+                Response.Write("Please select a file to upload.");
+            }
+        }
+    }
+}
